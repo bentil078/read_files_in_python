24d6bbfb4e5f77fae8953f5728c4b15291d5cd0f
Bentil078 <mensah.bentil@yahoo.com>
2021-08-20 23:44:22 +0000

    json files

---
+++ json-assign.py
@@ -0,0 +1,33 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+
+url = input('Enter url: ')
+if len(url) < 1:
+  url = 'http://py4e-data.dr-chuck.net/comments_42.json'
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url, context=ctx)
+data = uh.read().decode()
+print('Retrieved', len(data), 'characters')
+
+
+info = json.loads(data)
+
+# print(json.dumps(info, indent=4))
+
+sum = 0
+
+# Get the comments part of the dictionary
+print(len(info['comments']))
+
+for item in info["comments"]:
+    sum += int(item['count'])
+print(sum)
