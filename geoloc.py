711106430f90d94cc9f0f2c9c393acbeb49d153c
Bentil078 <mensah.bentil@yahoo.com>
2021-08-24 16:53:57 +0000

    sqlite3

--- geoloc.py
+++ geoloc.py
@@ -43,8 +43,8 @@ while True:
 
     print(js['results'][0]['place_id'])
 
-    # lat = js['results'][0]['geometry']['location']['lat']
-    # lng = js['results'][0]['geometry']['location']['lng']
-    # print('lat', lat, 'lng', lng)
-    # location = js['results'][0]['formatted_address']
-    # print(location)
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
